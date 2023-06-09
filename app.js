dd4a11dd8314dd72f7d7f129d73a16a33b5b247e
FacundoNicolasOviedo <facuoviedo80@gmail.com>
2023-06-08 23:54:41 -0300

    Arreglos primera pre entrega

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ let opcion;
 let nombreUsuario
 
 alert("Bienvenidos a TenisCenter, para continuar presione aceptar")
-
+function totalCarrito(total, precio, cantidad){
+    return total + precio * cantidad
+}
 nombreUsuario = prompt("ingrese su nombre")
 
 
 
 do {
-    raqueta = parseInt(prompt("elija el nombre de la raqueta que desea comprar")) 
+    raqueta = parseInt(prompt("elija el número de la raqueta que desea comprar:" + "\n" + "1- Wilson Pro Staff V13" + "\n" + "2- Head Speed 2022" + "\n" + "3- Babolat Aero Pro Drive" + "\n" + "4- Head Graphene")) 
+    while (raqueta >= 5) {
+        raqueta = prompt("Por favor ingrese una opción válida")
+    }
     precio = Number(prompt("Elija un precio"))
 
     while (precio <= 0) {
@@ -24,7 +29,7 @@ do {
         cantidad = prompt("Por favor ingrese una cantidad válida")
     }
     opcion = prompt ("Desea agregar mas productos? si/no")
-    total = total + precio * cantidad   
+    total = totalCarrito (total, precio, cantidad)
 }   while (opcion == "si")
 
 alert (nombreUsuario + " el total de su compra es de " + total + " pesos")
